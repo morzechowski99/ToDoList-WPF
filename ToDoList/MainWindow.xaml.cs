bcc6a201df2ea0752ab3703d6cdefeed134be6c8
Marcin Orzechowski <marcinorz@o2.pl>
2024-01-04 21:19:55 +0100

    Add project files.

---
+++ ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using ToDoList.ViewModels;
+using Task = ToDoList.DbModels.Task;
+
+namespace ToDoList;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly ObservableCollection<Task> _tasks = [];
+    private CollectionViewSource todosViewSource;
+    public NewTask Task { get; set; } = new();
+    public MainWindow()
+    {
+        InitializeComponent();
+        todosViewSource = (CollectionViewSource)FindResource(nameof(todosViewSource));
+        todosViewSource.Source = _tasks;
+    }
+
+    private void AddTaskButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (Validation.GetHasError(TaskNameTextBox))
+        {
+            StringBuilder sb = new();
+            Validation.GetErrors(TaskNameTextBox).ToList().ForEach(error => sb.AppendLine(error.ErrorContent.ToString()));
+            MessageBox.Show(sb.ToString(), "Form has errors", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+        _tasks.Add(new Task { Name = Task.Name!, IsComplete = false });
+        Task.Name = "";
+    }
+
+    private void OnDelete(object sender, ExecutedRoutedEventArgs e)
+    {
+        if ((sender as DataGrid)?.SelectedItem is Task task)
+        {
+            _tasks.Remove(task);
+        }
+    }
+}
---
+++ ToDoList/ViewModels/NewTask.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ToDoList.ViewModels;
+
+public class NewTask : IDataErrorInfo, INotifyPropertyChanged
+{
+    private string? _name;
+    public string? Name
+    {
+        get => _name; 
+        set { _name = value; OnPropertyChanged(); }
+    }
+    public string Error => string.Empty;
+    public string this[string columnName] => columnName == nameof(Name) && string.IsNullOrWhiteSpace(Name) ? "Name is required" : "";
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        if(PropertyChanged != null) PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
